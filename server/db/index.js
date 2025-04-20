ac2bb3f3713be661f7d88967bf5b6768ecff3f48
Rishabh Mishra <150777256+rishabhmishra007@users.noreply.github.com>
2025-04-20 21:09:25 +0530

    Update index.js

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
 mongoose.connect(
-    "mongodb+srv://rishabh23:XiWzx4KZta9A6mCl@cluster0.kclym.mongodb.net/"
+    "process.env.MONGO_URI"
   )
   .then(() => console.log("Connected mongo db"))
   .catch((err) => console.log(err));
